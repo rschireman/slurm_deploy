569123004ef64b6a865082104a7b624913c12ad1
Ray Schireman <41241487+rschireman@users.noreply.github.com>
2024-06-05 10:32:55 -0500

    initial

---
+++ job-sbatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=pcrystal
+#SBATCH --partition=bluemoon
+#SBATCH --array=1-500  # Change this to the desired array size
+#SBATCH --ntasks=48
+#SBATCH --time=8:00:00  # Adjust the time limit as needed
+
+
+
+cd run_${SLURM_ARRAY_TASK_ID}
+
+source /users/r/s/rschirem/intel/oneapi/setvars.sh
+/users/r/s/rschirem/CRYSTAL_23/ompi/bin/mpirun /users/r/s/rschirem/CRYSTAL_23/bin/Linux-ifort_openmpi_i64_clut/std/Pcrystal </dev/null &> output_${SLURM_ARRAY_TASK_ID}.out
